22cc5a4d594daf65fdd3ee8f1863421ba14ed19d
105753341 <105753341@student.swin.edu.au>
2025-05-21 17:22:50 +1000

    added manage.php and manger_login.php

---
+++ manage.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Header">
+    <meta name="keywords" content="HTML5, PHP">
+    <meta name="author" content="Harrison Strachan">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet/styles.css">
+
+</head>
+ 
+<body>
+    <h1>Manage EOIs</h1>
+
+    <form method="POST" action="manage.php">
+        <h2>List EOIs</h2>
+        <input type="submit" name="list_all" value="List All EOIs">
+
+        <h2>List EOIs by Job Reference</h2>
+        <label for="job_ref">Job Reference:</label>
+        <input type="text" id="job_ref" name="job_ref">
+        <input type="submit" value="Search by Job Reference">
+
+        <h2>List EOIs by Name</h2>
+        <label for="first_name">First Name:</label>
+        <input type="text" id="first_name" name="first_name">
+        <label for="last_name">Last Name:</label>
+        <input type="text" id="last_name" name="last_name">
+        <input type="submit" value="Search by Name">
+
+        <h2>Delete EOIs by Job Reference</h2>
+        <label for="delete_job_ref">Job Reference:</label>
+        <input type="text" id="delete_job_ref" name="delete_job_ref">
+        <input type="submit" value="Delete by Job Reference">
+
+        <h2>Update Status of an EOI</h2>
+        <label for="update_job_ref">EOI number:</label>
+        <input type="text" id="update_job_ref" name="update_job_ref">
+        <label for="statuss">Status:</label>
+        <select id="statuss" name="statuss">
+            <option value="New">New</option>
+            <option value="Current">Current</option>
+            <option value="Final">Final</option>
+        </select>
+        <input type="submit" value="Update Status">
+    </form>
+
+    <?php
+    // Requires a pre-existing database
+    require_once("settings.php");
+    // Open connection to database 
+    $conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if ($conn) {
+        $result = null;
+
+        if (isset($_POST['list_all'])) {         // If "List All" button is pressed, read all fields in EOI table
+
+            $query = "SELECT * FROM EOI";
+            $result = mysqli_query($conn, $query);
+        }
+        // Lists EOI according to entered job reference number
+        elseif (isset($_POST['job_ref']) && !empty($_POST['job_ref'])) {
+            $job_ref = mysqli_real_escape_string($conn, $_POST['job_ref']);
+            $query = "SELECT * FROM EOI WHERE job_reference_number LIKE '%$job_ref%'";
+            $result = mysqli_query($conn, $query);
+        }
+        // Lists all EOI from a selected first/last name
+        elseif (isset($_POST['first_name']) && !empty($_POST['first_name']) || isset($_POST['last_name']) && !empty($_POST['last_name'])) {
+            $first_name = mysqli_real_escape_string($conn, $_POST['first_name']);
+            $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
+            $query = "SELECT * FROM EOI WHERE first_name LIKE '%$first_name%' OR '%$last_name%'";
+            $result = mysqli_query($conn, $query);
+        }
+        // Deletes EOI according to entered job reference number
+        elseif (isset($_POST['delete_job_ref']) && !empty($_POST['delete_job_ref'])) {
+            $delete_job_ref = mysqli_real_escape_string($conn, $_POST['delete_job_ref']);
+            $query = "DELETE FROM EOI WHERE job_reference_number LIKE '%$delete_job_ref%'";
+            $result = mysqli_query($conn, $query);
+
+            if ($result) {
+                echo "<br>All EOI's with job reference number $delete_job_ref have been deleted. <br>";
+
+                // After deletion, selects all remaining records
+                $query = "SELECT * FROM EOI";
+                $result = mysqli_query($conn, $query);
+            } else {
+                echo "Error deleting EOI." . mysqli_error($conn);
+            }
+        }
+        // Updates status of EOI according to entered job reference number
+        elseif (isset($_POST['update_job_ref']) && !empty($_POST['update_job_ref']) && isset($_POST['statuss']) && !empty($_POST['statuss'])) {
+            $update_job_ref = mysqli_real_escape_string($conn, $_POST['update_job_ref']);
+            $status = mysqli_real_escape_string($conn, $_POST['statuss']);
+            $query = "UPDATE EOI SET statuss = '$status' WHERE eoi_number = '$update_job_ref'";
+            $result = mysqli_query($conn, $query);
+
+            if ($result) {
+                echo "<br>Status updated for EOI number: $update_job_ref. <br> ";
+
+                // After the update, selects the updated record
+                $query = "SELECT * FROM EOI WHERE eoi_number = '$update_job_ref'";
+                $result = mysqli_query($conn, $query);
+            } else {
+                echo "Error updating status." . mysqli_error($conn);
+            }
+        }
+
+
+        // Creates table if result returns "1"
+        if ($result && mysqli_num_rows($result) > 0) {
+            echo "<br><table border='1'>
+                    <thead>
+                    <tr><th>EOI</th></tr>
+                    </thead>
+                        <tr>
+                            <th>EOI Number</th>
+                            <th>Job Reference</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>DOB</th>
+                            <th>Gender</th>
+                            <th>Street Address</th>
+                            <th>Suburb</th>
+                            <th>State</th>
+                            <th>Postcode</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Other Skills</th>
+                            <th>Status</th>
+                        </tr>";
+            //Fetches results from SQL database and prints to table
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>
+                            <td>" . $row["eoi_number"] . "</td>
+                            <td>" . $row["job_reference_number"] . "</td>
+                            <td>" . $row["first_name"] . "</td>
+                            <td>" . $row["last_name"] . "</td>
+                            <td>" . $row["date_of_birth"] . "</td>
+                            <td>" . $row["gender"] . "</td>
+                            <td>" . $row["street_address"] . "</td>
+                            <td>" . $row["suburb"] . "</td>
+                            <td>" . $row["state"] . "</td>
+                            <td>" . $row["postcode"] . "</td>
+                            <td>" . $row["email"] . "</td>
+                            <td>" . $row["phone"] . "</td>
+                            <td>" . $row["other_skills"] . "</td>
+                            <td>" . $row["statuss"] . "</td>
+                        </tr>";
+            }
+            echo "</table>";
+        }
+
+        //Connection to database closed
+        mysqli_close($conn);
+    }
+    //If unable to connect to database
+    else {
+        echo "<p>Unable to connect to the database..</p>";
+    }
+    ?>
+
+</body>
+
+</html>
---
+++ manager_login.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Header">
+    <meta name="keywords" content="HTML5, PHP">
+    <meta name="author" content="Harrison Strachan">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet/styles.css">
+    
+</head>
+<body>
+    <h1>Manager LOGIN</h1>
+
+    <form method="post" action="login_manager.php">
+        
+        <label for="username">Username:</label>
+        <input type="text" name="username" required>
+        
+        <br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" required>
+
+        <br>
+
+        <input type="hidden" name="token" value="manager">
+        <input type="submit" value="Login">
+    </form>
+
+    <?php
+        session_start();
+        // Requires a pre-existing database
+        require_once("settings.php");
+        // Open connection to database 
+        $conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if($conn) {
+                //Puts entered username and pwd into variables
+                $username = mysqli_real_escape_string($conn, $_POST['username']);
+                $password = mysqli_real_escape_string($conn, $_POST['password']);
+            
+                $query = "SELECT * FROM managers WHERE username = '$username'";
+                $result = mysqli_query($conn, $query);
+
+                if ($result && mysqli_num_rows($result) > 0) {
+                    
+                    $row = mysqli_fetch_assoc($result);
+
+                    if($username == $row['username'] && $password == $row['password']) {
+                        $_SESSION['username'] = $username;
+                        $_SESSION['password'] = $password;
+
+                        header("Location: manage.php"); //redirects to manager page (manage.php)
+                        exit();
+                    }
+                    else {
+                        echo "<p>Invalid username or password.</p>"; 
+                        //prints invalid username or password on failed log in attempt
+                    }
+                }
+
+                mysqli_close($conn); //ends connection to db
+            }
+        }
+    ?>
+</body>
+</html>
