b6dbd75ae8c8b63084143836c9c6cb5601dd9667
Alexander <105873100@student.swin.edu.au>
2025-05-26 18:18:31 +1000

    better spacing/padding for index page

--- enhancements.php
+++ enhancements.php
@@ -16,14 +16,14 @@
     <h1 class="main-heads">Enhancements</h1>
     <hr>
 
-    <!-- inherited styles from about page, since style of this page is very similar, not need to write new css -->
+    <!-- inherited styles from about page, since style of this page is very similar, no need to write new css -->
     <main id="about-main">
     <!-- Generic "who we are" section, not required -->
     <section class="about-section">
             <h2>Implemented suggested changes</h2>
             <!-- The following was generated using copilot AI for a generic "who we are", the prompt was: "create a generic "about us" section for a company called "tech nova"" -->
             <div>
-                <ul>
+                <ul id="improvements">
                     <li>Increase whitespace / break up text in the index and jobs pages</li>
                     <li>Meta description/keywords for each page</li>
                     <li>Reduce image sizes to less than 300kb to improve peformance</li>
