9b19ded137dbab0c3722488992c3de0bf536237c
Alexander <105873100@student.swin.edu.au>
2025-05-26 15:51:16 +1000

    major changes to manager_login, added session support, fixed broken links

---
+++ process_login.php
@@ -0,0 +1,45 @@
+<?php
+    session_start();
+    require_once("settings.php"); //opens the connection to the db
+    $conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if (!$conn) {
+        die("Database connection failed: " .mysqli_connect_error()); //if connection failed print generic error + error returned
+        }   
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $username = trim(mysqli_real_escape_string($conn, $_POST['username']));
+            $password = trim(mysqli_real_escape_string($conn, $_POST['password']));
+        
+            $query = "SELECT * FROM users WHERE username = '$username'";
+            $result = mysqli_query($conn, $query);
+
+            if ($result && mysqli_num_rows($result) > 0) {
+                
+                $row = mysqli_fetch_assoc($result);
+
+                if($username == $row['username'] && $password == $row['password']) {
+                    $_SESSION['username'] = $username;
+                    $_SESSION['password'] = $password; //compares input to db
+
+                    header("Location:manage.php"); //redirects to managers page (manage.php)
+                    exit;
+                    //die();
+                }
+                else {
+                    $_SESSION['error'] = "Invalid username or password. Please try again";
+                    header('Location: manager_login.php');
+                    exit;
+                    //prints invalid username or password on failed log in attempt
+                }
+            }
+            
+             else {
+                    $_SESSION['error'] = "Invalid username or password. Please try again";
+                    header('Location: manager_login.php');
+                    exit;
+                    //prints invalid username or password on failed log in attempt
+                }
+            //mysqli_close($conn); //ends connection to db
+        }
+?>
